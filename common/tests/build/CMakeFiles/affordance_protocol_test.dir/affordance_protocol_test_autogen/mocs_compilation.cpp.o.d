46e913ad9d9e281f3b02ec334866729d4dd6eb19
pbustos <pbustos@unex.es>
2026-05-28 12:19:04 +0200

    remove build

--- common/tests/build/CMakeFiles/affordance_protocol_test.dir/affordance_protocol_test_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/affordance_protocol_test.dir/affordance_protocol_test_autogen/mocs_compilation.cpp.o: \
- /home/pbustos/robocomp/components/active_inference/common/tests/build/affordance_protocol_test_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- common/tests/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pbustos/robocomp/components/active_inference/common/tests
-# Build directory: /home/pbustos/robocomp/components/active_inference/common/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(affordance_protocol_test "/home/pbustos/robocomp/components/active_inference/common/tests/build/affordance_protocol_test")
-set_tests_properties(affordance_protocol_test PROPERTIES  _BACKTRACE_TRIPLES "/home/pbustos/robocomp/components/active_inference/common/tests/CMakeLists.txt;73;add_test;/home/pbustos/robocomp/components/active_inference/common/tests/CMakeLists.txt;0;")
