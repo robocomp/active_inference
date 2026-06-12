a39fc991e1b06b6563e13e4ae26e93e7df6ee291
pbustos <pbustos@unex.es>
2026-06-12 12:05:28 +0200

    added file

---
+++ robot_concept/src/specificworker_presence.cpp
@@ -0,0 +1,50 @@
+#include "specificworker.h"
+
+void SpecificWorker::waiting_enter()
+{
+    presence_coordinator_.waiting_enter();
+}
+
+void SpecificWorker::waiting_loop()
+{
+    presence_coordinator_.waiting_loop();
+}
+
+void SpecificWorker::operating_enter()
+{
+    presence_coordinator_.operating_enter();
+}
+
+void SpecificWorker::operating_loop()
+{
+    presence_coordinator_.operating_loop();
+}
+
+void SpecificWorker::degraded_enter()
+{
+    presence_coordinator_.degraded_enter();
+}
+
+void SpecificWorker::degraded_loop()
+{
+    presence_coordinator_.degraded_loop();
+}
+
+void SpecificWorker::cleanup_owned_nodes()
+{
+    if (owned_nodes_cleaned_)
+        return;
+    owned_nodes_cleaned_ = true;
+
+    presence_coordinator_.cleanup_owned_nodes();
+}
+
+void SpecificWorker::on_optional_peer_lost(const std::string &name, std::uint32_t /*id*/)
+{
+    qInfo() << "[Presence] optional peer lost:" << QString::fromStdString(name);
+}
+
+void SpecificWorker::on_optional_peer_ready(const std::string &name, std::uint32_t /*id*/)
+{
+    qInfo() << "[Presence] optional peer ready:" << QString::fromStdString(name);
+}
